32535659ed3c4d961c45ebbcb1cc6062c7b27ccc
miaaaaaaaa1 <solomia1pochaievets@gamil.com>
2025-09-29 17:52:57 +0300

    Classwork

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace _02_TaskManager
+{
+    public class ProcessInfo
+    {
+        public int Id { get; set; }
+        public string ProcessName { get; set; }
+        public string StartTime { get; set; }
+        public bool Responding { get; set; }
+        public int BasePriority { get; set; }
+        public Process ProcessRef { get; set; }
+    }
+    private List<ProcessInfo> GetProcessList()
+    {
+            var list = new List<ProcessInfo>();
+            foreach (var proc in Process.GetProcesses())
+            {
+                string startTime = "";
+                try { startTime = proc.StartTime.ToString(); } catch { }
+                list.Add(new ProcessInfo
+                {
+                    Id = proc.Id,
+                    ProcessName = proc.ProcessName,
+                    StartTime = startTime,
+                    Responding = proc.Responding,
+                    BasePriority = proc.BasePriority,
+                    ProcessRef = proc
+                });
+            }
+            return list;
+    }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = GetProcessList();
+        }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+            //(grid.SelectedItem as Process)
+            string message = " ";
+            message += "Hellloooooooooo";
+
+            MessageBox.Show(message, "Process info", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show(nameProc.Text);
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_TaskManager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+            //(grid.SelectedItem as Process)
+            string message = " ";
+            message += "Hellloooooooooo";
+
+            MessageBox.Show(message, "Process info", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show(nameProc.Text);
+        }
+        private void ShowInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+            Process selectedProcess = (grid.SelectedItem as Process);
+            string info = $"Process Name: {selectedProcess.ProcessName}\n";
+                //              $"PID: {selectedProcess.Id}\n" +
+                //              $"Start Time: {selectedProcess.StartTime}\n" +
+                //              $"Priority: {selectedProcess.PriorityClass}\n" +
+                //              $"Total Processor Time: {selectedProcess.TotalProcessorTime}\n";
+                MessageBox.Show(info, "Process Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void Kill_Click(object sender, RoutedEventArgs e)
+        {
+            if (grid.SelectedItem is Process selectedProcess)
+            {
+                    selectedProcess.Kill();
+                    MessageBox.Show($"Process {selectedProcess.ProcessName} (PID: {selectedProcess.Id}) terminated.", "Process Killed", MessageBoxButton.OK, MessageBoxImage.Information);
+                    grid.ItemsSource = Process.GetProcesses();
+            }
+        }
+    }
+}
